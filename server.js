1ba709a3fbb07ac56ea6cb7c14b3211201599f06
Sahar11 <saharhalim151@gmail.com>
2023-04-13 17:44:53 -0400

    add header

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app =new express();
 const port = 8080;
+app.use(express.static('public')) //enable everthing in the folder as static file
 app.listen(port, ()=>{
   console.log(`Serving photo app on http://localhost:${port}`)
 });
