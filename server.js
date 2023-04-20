5a55ad8eaeba030973ac8f56b59e6bb7fc0c4e6d
Sahar11 <saharhalim151@gmail.com>
2023-04-20 16:39:04 -0400

    add sequelize

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+// const { response } = require('express')
 const express = require('express');
-const app =new express();
+const app =express();
 const port = 8080;
 app.use(express.static('public')) //enable everthing in the folder as static file
+app.set('view engine', 'ejs')
+
+app.get('/', (request, response) => {
+  response.render("index")
+});
+
 app.listen(port, ()=>{
   console.log(`Serving photo app on http://localhost:${port}`)
-});
+}); 
