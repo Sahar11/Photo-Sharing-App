0525cd35fb9c18e8c243555662092e0ce756ee91
Sahar11 <saharhalim151@gmail.com>
2023-05-02 15:48:27 -0400

    add multer

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app =express();
 const db = require('./models');
 const bodyParser = require('body-parser');
 
+app.use(bodyParser.json())
 app.use(express.static('public')) //enable everthing in the folder as static file
 app.set('view engine', 'ejs')
 
@@ -15,11 +16,19 @@ app.get('/', (request, response) => {
 const sqlPort = 3307; // 3306 or 3307
 
 // sequelize way
+db.sequelize
+    .sync({})
+    .then(()=>{
+      app.listen(sqlPort, () => {
+        console.log(
+          `Mariadb Connection Successful - http://localhost:${sqlPort}`
+          )
+      })
+    })
+    .catch((error)=> {
+      console.error("Unable to connect to the database", error)
+    })
 
-//traditional sql way
-// app.listen(sqlPort, () => {
-//   console.log(`Mariadb Connection Successful - http://localhost:${sqlPort}`)
-// })
 
 //server
 const port = 8080;
